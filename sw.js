a66e787f39f23db3a4aa9b0adcb1ccec39571801
MFBIGMARKET <mfbigmarketiritty@gmail.com>
2025-10-31 19:49:13 +0530

    initial commit

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'metro-fresh-po-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo-192.png',
+  './logo-512.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2' // This is the Material Icons font file
+];
+
+// Install event: cache the app shell
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache and caching app shell');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: serve from cache first, then network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // Not in cache - go to network
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event: clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
